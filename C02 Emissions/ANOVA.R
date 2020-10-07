5ffbcecd1b7e0c049671611b93de0c10fc50f7c4
Noah <noahsolomon0518@gmail.com>
2020-10-07 15:10:43 -0400

    fish regression

--- C02 Emissions/ANOVA.R
+++ C02 Emissions/ANOVA.R
@@ -1,2 +1,24 @@
 mydata = read.csv("co2_emission.csv")
 print(mydata)
+
+
+
+dataGroupBy <- function(country){
+  data <- mydata[mydata[,"Entity"]==country,]
+  data <- data[data[,4]!=0,]
+  return(reindex(data))
+}
+
+
+AfricaC02 <- dataGroupBy("Africa")
+attach(AfricaC02)
+
+test <- AfricaC02[,3]^(1/2)
+plot(AfricaC02[,4]~test)
+
+
+mod = lm(AfricaC02[,4]~AfricaC02[,3])
+
+summary(mod)
+
+plot(mod)
